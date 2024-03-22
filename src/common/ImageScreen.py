efabc2e7a62bb34ca2ae20e59fd929aa16c38f52
Febian <febian@renpho.com>
2024-03-22 11:49:27 +0800

    feat: add filter check

--- src/common/ImageScreen.py
+++ src/common/ImageScreen.py
@@ -13,39 +13,55 @@ IMAGE_URL = r"C:/Users/11947/Desktop/3.png"
 class ImageScreen(object):
     pass
 
+    def __init__(self):
+        self.filter_list= ['shiny', 'Suicune', 'Entei', 'Raikou', 'Zapdos', 'Articuno', 'Moltres']
+        self.filter_list_zh= ['闪光', '水君', '炎帝', '雷公', '闪电鸟', '急冻鸟', '火焰鸟']
 
     def getScreenShot(self, topX, topY, width, high, url):
         # img = pyautogui.screenshot()    #返回值为一个Img对象
         img = pyautogui.screenshot(url,region=(topX,topY,width,high))
         img.save(url)
 
+    def addFiltItem(self, item):
+        self.filter_list_zh.append(item)
+
     # 安装tesseract ： https://digi.bib.uni-mannheim.de/tesseract/
     # 语言包安装https://tesseract-ocr.github.io/tessdoc/Data-Files
     # 隐藏maxvit中的OrderedDict的包 pip install collective.ordereddict
-    def recognition_img(self, url):
+    def recognition_img(self, url, target):
         # pytesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
         # pytesseract.pytesseract.tesseract_cmd = pytesseract_cmd
-        img = Image.open(url)
+        # img = Image.open(url)
         # text = pytesseract.image_to_string(img, lang='chi_sim')
 
-        reader = easyocr.Reader(['ch_sim', 'en'],gpu=False)
-        text = reader.readtext(url,detail=0)
+        reader = easyocr.Reader(['ch_sim', 'en'], gpu=False)
+        text = reader.readtext(url, detail=0)
 
         print(text)
-        hasShiny = False
+        has_target = False
         for item in text:
-            if not hasShiny and '闪光' in item:
-                print(item)
-                hasShiny = True
-                break
-        return hasShiny
+            if not has_target:
+                for tar in target:
+                    if not has_target and tar in item:
+                        has_target = True
+                        break
+        return has_target
 
     def check_shiny(self):
         time.sleep(1)
         c = ImageScreen()
         c.getScreenShot(0,750, 500, 300, IMAGE_URL)
-        return c.recognition_img(IMAGE_URL)
+        target = [self.filter_list[0], self.filter_list_zh[0]]
+        print("filter list :  %s " % (target))
+        return c.recognition_img(IMAGE_URL, target)
 
+    def check_default_list(self):
+        time.sleep(1)
+        c = ImageScreen()
+        c.getScreenShot(0,750, 500, 300, IMAGE_URL)
+        target = self.filter_list_zh + self.filter_list
+        print("filter list :  %s " % (target))
+        return c.recognition_img(IMAGE_URL, target)
 
         # rInt = random.randint(1,10)
         # print('random int %d' % rInt)
@@ -56,7 +72,8 @@ class ImageScreen(object):
 
 
 # c = ImageScreen()
-# c.getScreenShot()
+# c.addFiltItem('TestStr')
+# c.check_default_list()
 # print(c.check_shiny())
 # print(c.check_shiny())
 # print(c.check_shiny())
